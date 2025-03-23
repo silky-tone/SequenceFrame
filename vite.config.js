695f41a1168d76ff55b217c2afdafba099c46f82
ReFunny <2048412928@qq.com>
2025-03-23 23:11:56 +0800

    build config

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,23 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
-import Package from './package.json';
 
-const PackageName = Package.name.split('-').map(item => {
-  return item.trim();
-}).filter(item => {
-  return Boolean(item);
-}).map(item => {
-  return item ? item[0].toUpperCase() + item.slice(1) : '';
-}).join('');
-
-export default defineConfig({
-  root: resolve(__dirname, './example'),
-  server: {
-    port: 8415,
-    open: true,
-  },
-  publicDir: resolve(__dirname, './public'),
-  build: {
-    target: 'chrome74',
-    outDir: resolve(__dirname, './dist'),
-    lib: {
-      name: PackageName,
-      fileName: Package.name,
-      formats: ['umd', 'es'],
-      entry: resolve(__dirname, './lib/index.ts'),
+export default defineConfig(({ command }) => {
+  return {
+    root: resolve(__dirname, './example'),
+    server: {
+      port: 8415,
+      open: true,
     },
-  },
+    publicDir: command === 'build' ? false : resolve(__dirname, './public'),
+    build: {
+      target: 'chrome74',
+      outDir: resolve(__dirname, './dist'),
+      lib: {
+        fileName: 'index',
+        name: 'SequenceFrame',
+        formats: ['umd', 'es'],
+        entry: resolve(__dirname, './lib/index.ts'),
+      },
+    },
+  };
 });
